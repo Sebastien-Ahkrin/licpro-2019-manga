f202a87220f97081de75a026e5f7d459ecb6d0ab
lebonnet5 <lebonnet5@iutclinfa1009l.iut.local>
2019-01-09 17:27:47 +0100

    Add CRUD

--- back/src/index.js
+++ back/src/index.js
@@ -1,1 +1,36 @@
 const server = require('./Server')
+
+/**
+ *
+ */
+server.post("/episodes", function(req, res) {
+
+})
+
+/**
+ *
+ */
+server.get("/episodes", function(req, res) {
+
+})
+
+/**
+ *
+ */
+server.get("/episodes/:id", function(req, res) {
+    console.log(req.params.id)
+})
+
+/**
+ *
+ */
+server.put("/episodes/:id", function(req, res) {
+
+})
+
+/**
+ *
+ */
+server.delete("/episode/:id", function (req, res) {
+
+})
