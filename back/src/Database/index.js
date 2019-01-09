26db691de95f7f804177462d051f074407d088d1
Sébastien Criquet <sebastien48criquet@gmail.com>
2019-01-09 22:29:08 +0100

    Reformat code

--- back/src/Database/index.js
+++ back/src/Database/index.js
@@ -2,7 +2,7 @@ const { promisify } = require('util')
 const fs = require('fs')
 
 //Path of db folder
-const path = __dirname + '/../../db'
+const path = `${ __dirname }/../../db`
 
 //Transform this method in a Promise
 const readFileAsync = promisify(fs.readFile)
@@ -11,52 +11,27 @@ const readDirAsync = promisify(fs.readdir)
 /**
  * Remove every space and remplace it by a -. (if its a string)
  * If its a number just add "0" if the number is less than 10.
- * @param { string || number} value 
+ * @param { string | number} value 
  */
-const transform = value => {
-  if((typeof value === 'number') && (value <= 9)) {
-    value = `0${value}`
-  } else {
-    value = value.replace(/[ ]/g, '-')
-  }
-  return value
-}
+const transform = value => ((typeof value === 'number') && (value <= 9) ? `0${ value }` : value.replace(/[ ]/g, '-'))
 
 /**
  * Return every manga name in a Array
  */
-const getMangas = () => {
-  return readDirAsync(`${ path }`).catch(error => {
-    console.error(`An error occured here #getMangas():  ${ error }`)
-    Promise.reject(error)
-  })
-}
+const getMangas = async () => await readDirAsync(path)
 
 /**
  * Return the list of all the seasons of a mangas name
  * @param { string } name 
  */
-const getSeasons = name => {
-  return readDirAsync(`${ path }/${ transform(name) }`).catch(error => {
-    console.error(`An error occured here #getSeasons():  ${ error }`)
-    Promise.reject(error)
-  })
-}
-
+const getSeasons = name => readDirAsync(`${ path }/${ transform(name) }`)
 /**
  * Get the json of the episode
  * @param { string } name 
  * @param { number } season 
  * @param { number } episode 
  */
-const getEpisode = (name, season, episode) => {
- return readFileAsync(`${path}/${ transform(name) }/Season-${ transform(season) }/Episode-${ transform(episode) }.json`, 'utf8').then(buffer => {
-    return buffer
-  }).catch(error => {
-    console.error(`An error occured here #getEpisode():  ${ error }`)
-    Promise.reject(error)
-  })
-}
+const getEpisode = (name, season, episode) => readFileAsync(`${path}/${ transform(name) }/Season-${ transform(season) }/Episode-${ transform(episode) }.json`, 'utf8')
 
 module.exports = {
   transform,
