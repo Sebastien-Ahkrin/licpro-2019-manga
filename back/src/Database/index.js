6bf477a540793e43afef944b335d344e8a4e8927
Sébastien Criquet <sebastien48criquet@gmail.com>
2019-01-10 21:36:10 +0100

    Adding a new function write test after creating a delete function

--- back/src/Database/index.js
+++ back/src/Database/index.js
@@ -7,35 +7,64 @@ const path = `${ __dirname }/../../db`
 //Transform this method in a Promise
 const readFileAsync = promisify(fs.readFile)
 const readDirAsync = promisify(fs.readdir)
+const createFile = promisify(fs.writeFile)
+const mkdir = promisify(fs.mkdir)
+
+/**
+ * Return a new string from this 3 strings
+ * @param { string } name 
+ * @param { number } season 
+ * @param { number } episode 
+ */
+const stringify = (name, season, episode) => `${ path }/${ transform(name) }/Season-${ transform(season) }/Episode-${ transform(episode) }.json`
 
 /**
  * Remove every space and remplace it by a -. (if its a string)
  * If its a number just add "0" if the number is less than 10.
- * @param { string | number} value 
+ * @param { string | number } value 
  */
 const transform = value => ((typeof value === 'number') && (value <= 9) ? `0${ value }` : value.replace(/[ ]/g, '-'))
 
 /**
  * Return every manga name in a Array
  */
-const getMangas = async () => await readDirAsync(path)
+const getMangas = () => readDirAsync(path)
 
 /**
  * Return the list of all the seasons of a mangas name
  * @param { string } name 
  */
 const getSeasons = name => readDirAsync(`${ path }/${ transform(name) }`)
+
 /**
  * Get the json of the episode
  * @param { string } name 
  * @param { number } season 
  * @param { number } episode 
  */
-const getEpisode = (name, season, episode) => readFileAsync(`${path}/${ transform(name) }/Season-${ transform(season) }/Episode-${ transform(episode) }.json`, 'utf8')
+const getEpisode = (name, season, episode) => readFileAsync(stringify(name, season, episode), 'utf8')
+
+/**
+ * Create a file with this object : { grade: x }
+ * x = number
+ * @param { string } name 
+ * @param { number } season 
+ * @param { number } episode 
+ * @param { object } data 
+ */
+const createManga = async (name, season, episode, data) => {
+  await mkdir(`${ path }/${ transform(name) }/Season-${ transform(season) }`, { recursive: true })
+  await createFile(stringify(name, season, episode), JSON.stringify(data)).catch(console.error)
+}
+
 
 module.exports = {
-  transform,
-  getEpisode,
-  getMangas,
-  getSeasons
+  read: { 
+    getEpisode, 
+    getMangas, 
+    getSeasons 
+  },
+  create: {
+    createManga
+  }
 }
--- back/test/index.spec.js
+++ back/test/index.spec.js
@@ -1,30 +1,38 @@
 const assert = require('assert')
 
-const { transform, getEpisode, getMangas, getSeasons } = require('../src/Database')
+const { 
+  read: { 
+    getEpisode, 
+    getSeasons, 
+    getMangas 
+  },
+  create: {
+    createManga
+  }
+} = require('../src/Database')
 
 describe('Database', () => {
-  describe('#transform()', () => {
-    it('should transform the number or string by a good prototype', () => {
-      assert.equal(transform('No Game No Life'), 'No-Game-No-Life')
-      assert.equal(transform(1), '01')
-    })
-  })
-
-  describe('#getEpisode()', () => {
+  describe('read#getEpisode()', () => {
     it('should return the grade of a episode', async () => {
       assert.equal(await getEpisode('No Game No Life', 1, 1), "{\r\n  \"grade\": 8\r\n}")
     })
   })
 
-  describe('#getMangas()', () => {
+  describe('read#getMangas()', async () => {
     it('should return the list of every mangas in array', async () => {
       assert.deepStrictEqual(await getMangas(), ['Goblin-Slayer', 'No-Game-No-Life'])
     })
   })
 
-  describe('#getMangas()', () => {
+  describe('read#getMangas()', () => {
     it('should return the list of every mangas in array', async () => {
       assert.deepStrictEqual(await getSeasons('Goblin Slayer'), ['Season-01'])
+    })
+  })
+
+  describe('create#createManga()', () => {
+    it('should create a new mangas with grade: 7', async () => {
+      //Test this when you can remove a new file
     })
   })
 })
