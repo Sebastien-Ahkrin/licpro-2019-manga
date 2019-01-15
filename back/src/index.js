090e1228cbb6c13ce0985b36af21b00362b5384f
Sébastien Criquet <sebastien48criquet@gmail.com>
2019-01-15 15:50:01 +0100

    Adding comments

--- back/src/index.js
+++ back/src/index.js
@@ -1,5 +1,16 @@
 const server = require('./Server')
 
+/*
+[GET] /mangas => Donne tous les mangas
+[GET] /mangas/:name => Donne toutes les saisons d'un mangas
+[GET] /mangas/:name/:season => Donne la liste de tout les épisodes
+[GET] /mangas/:name/:season/:episode => Donne la note d'un episode
+
+[DELETE] /mangas/:name => Supprimer un mangas
+[PUT] /mangas/:name/:season/:episode | DATA dans le header ? => Creer un mangas
+[POST] /mangas/:name/:season/:episode | DATA dans le header ?  => Modifie un mangas
+*/
+
 /**
  *
  */
