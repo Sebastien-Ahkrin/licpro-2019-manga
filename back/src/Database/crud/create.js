dbcd0162c91930f8dbd993cbe1357151bd54d6b2
Sébastien Criquet <sebastien48criquet@gmail.com>
2019-01-15 16:45:20 +0100

    Solbve bug from node 8.x

--- back/src/Database/crud/create.js
+++ back/src/Database/crud/create.js
@@ -1,11 +1,11 @@
 const { stringify, transform, path } = require('../util')
 
 const { promisify } = require('util')
-const fs = require('fs')
+const fs = require('fs-extra')
 
 //Transform this method in a Promise
 const createFile = promisify(fs.writeFile)
-const mkdir = promisify(fs.mkdir)
+const mkdir = promisify(fs.ensureDir)
 
 /**
  * Create a file with this object : { grade: x }
@@ -18,6 +18,12 @@ const mkdir = promisify(fs.mkdir)
 const createManga = async (name, season, episode, data) => {
   await mkdir(`${ path }/${ transform(name) }/Season-${ transform(season) }`, { recursive: true })
   await createFile(stringify(name, season, episode), JSON.stringify(data))
+}
+
+const test = path => {
+  return new Promise((resolve, reject) => {
+    console.log(path)
+  })
 }
 
 module.exports = {
