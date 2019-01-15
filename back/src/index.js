2de17080161fc6ab3494baae33e22810689797e7
lebonnet5 <lebonnet5@iutclinfa1009l.iut.local>
2019-01-15 17:18:22 +0100

    Add some GET functions

--- back/src/index.js
+++ back/src/index.js
@@ -1,4 +1,5 @@
 const server = require('./Server')
+const { read, create, deleteM, update} = require('./Database')
 
 /*
 [GET] /mangas => Donne tous les mangas
@@ -12,36 +13,38 @@ const server = require('./Server')
 */
 
 /**
- *
+ * Get all the mangas
  */
-server.post("/episodes", function(req, res) {
-
+server.get("/mangas", async function ({ params }, res) {
+  const mangas = await read.getMangas()
+  res.json(mangas)
 })
 
 /**
- *
+ * 
  */
-server.get("/episodes", function(req, res) {
+server.get("/mangas/:name", async function ({ params }, res) {
+  const seasons = await read.getSeasons(req.params.name)
+  res.json(seasons)
+})
+
+server.get("/mangas/:name/:season", async function ({ params }, res) {
 
 })
 
-/**
- *
- */
-server.get("/episodes/:id", function(req, res) {
-    console.log(req.params.id)
+server.get("/mangas/:name/:season/:episode", async function ({ params }, res) {
+  const episode = await read.getEpisode(params.name, params.season, params.episode)
+  res.json(episode)
 })
 
-/**
- *
- */
-server.put("/episodes/:id", function(req, res) {
-
+server.delete("/mangas/:name", async function (req, res) {
+  
 })
 
-/**
- *
- */
-server.delete("/episode/:id", function (req, res) {
+server.put("/mangas/:name/:season/:episode", async function (req, res) {
+  
+})
 
+server.post("/mangas/:name/:season/:episode", async function (req, res) {
+  
 })
