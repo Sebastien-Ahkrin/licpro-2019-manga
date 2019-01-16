014057b971e3c5ef513f833f37f1d59a71418161
Sébastien Criquet <sebastien48criquet@gmail.com>
2019-01-16 16:08:28 +0100

    Update Middleware doc

--- back/src/Middleware/index.js
+++ back/src/Middleware/index.js
@@ -24,7 +24,7 @@ const use = middleware => server.use(middleware)
  * 
  * uses(middlewares)
  * 
- * @param { Function } middlewares 
+ * @param { Array } middlewares 
  */
 const uses = middlewares => middlewares.forEach(middleware => use(middleware))
 
