567992abea6db81f56b306e78f0ed69a4c8a2db6
Sébastien Criquet <sebastien48criquet@gmail.com>
2019-02-06 16:31:17 +0100

    Add delete button

--- front/src/components/List/ListItem/ListItem.js
+++ front/src/components/List/ListItem/ListItem.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react'
+
 import './ListItem.css'
 
 class ListItem extends Component {
 
   render() {
-    const { episode } = this.props
+    const { episode, callback } = this.props
     const [ , season, ep ] = episode.code.split(/[S|E]/g)
 
     return (
       <tr className='ListItem'>
-        <td>{ episode.id }</td>
         <td>{ episode.name }</td>
         <td>{ season }</td>
         <td>{ ep }</td>
+        <td>{ episode.grade }</td>
+        <td><span className='btn btn-primary' onClick={ () => callback() }>x</span></td>
       </tr>
     )
   }
