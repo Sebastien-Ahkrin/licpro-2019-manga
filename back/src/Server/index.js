241d900ad48976e475b98f839460756e08f90dba
Sébastien Criquet <sebastien48criquet@gmail.com>
2019-02-06 15:23:23 +0100

    Remove comments ..

--- back/src/Server/index.js
+++ back/src/Server/index.js
@@ -11,17 +11,6 @@ const { createManga } = require('./../Database/crud/create')
 const port = process.env.PORT || 8080
 
 app.listen(port, async () => {
-  //console.log(`regenerate with [2] new data`)
-
-  /*listAll().then(list => {
-    list.forEach(async (episode) => await deleteManga(episode.replace('.json', '')))
-  })*/
-
-  //const a = await createManga({ name: "Death Note", grade: 10, code: "S01E01", id: uuid() })
-  //const b = await createManga({ name: "Mirai Nikki", grade: 9, code: "S01E01", id: uuid() })
-
-  //console.log(`[1]: { name: "Death Note", grade: 10, code: "S01E01", id: "${ a }"} [CREATED]`)
-  //console.log(`[2]: { name: "Mirai Nikki", grade: 9, code: "S01E01", id: "${ b }"} [CREATED]`)
   console.log(`\nServer listening on ${ port } \nhttp://localhost:${ port }`)
 })
 
