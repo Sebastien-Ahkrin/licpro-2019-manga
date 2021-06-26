4c74c4a82c3a77a176f8cbe84c46a06935c87702
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2021-06-26 16:45:11 +0200

    feat: create context & dispatch

---
+++ front/src/context/useApp.tsx
@@ -0,0 +1,64 @@
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  useContext,
+  useReducer,
+} from "react";
+import { add } from "../actions";
+
+export interface Episode {
+  name: string;
+  grade: number;
+  episode: number;
+  code: string;
+  id: string;
+}
+
+export type ActionType<Action, Payload = void> = Payload extends void
+  ? { type: Action }
+  : { type: Action; payload: Payload };
+
+type AppActions =
+  | ActionType<"ADD_EPISODE", { episode: Episode }>
+  | ActionType<"LOAD_EPISODES", Array<Episode>>;
+
+type AppDispatch = Dispatch<AppActions>;
+
+interface AppState {
+  episodes: Array<Episode>;
+}
+
+const Context = createContext<[AppState, AppDispatch] | null>(null);
+const initial: AppState = {
+  episodes: [],
+};
+
+export default function useApp(): [AppState, AppDispatch] {
+  const context = useContext(Context);
+
+  if (!context) {
+    throw new Error("context doesnt have any initializer");
+  }
+
+  return context;
+}
+
+function reducer(previous: AppState, actions: AppActions): AppState {
+  switch (actions.type) {
+    case "ADD_EPISODE": {
+      void add(actions.payload.episode);
+      return { episodes: [...previous.episodes, actions.payload.episode] };
+    }
+    case "LOAD_EPISODES": {
+      return { episodes: actions.payload };
+    }
+    default:
+      throw new Error("Not implemented yet");
+  }
+}
+
+export function AppProvider(props: { children: ReactNode }) {
+  const ctx = useReducer(reducer, initial);
+  return <Context.Provider value={ctx}>{props.children}</Context.Provider>;
+}
